b44d2839361dd6ae5d3224eed5e0db6d2182a439
Bill Kalominidis <billkalominidis@gmail.com>
2015-01-25 09:12:50 +1100

    Update

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -29,39 +29,64 @@ public class PlayerBehaviour : MonoBehaviour, Noun
 
 	private float timer = 0;
 	public float GlobalCooldown = 0.25f;
-	
+	private bool isStunned = false;
+	private float stunnedTimer = 0.0f;
+
+	public Sprite StunnedSprite;
+	private Sprite normalSprite;
+	private SpriteRenderer spriteRenderer;
+	private Animator animator;
+
 	void Start()
 	{
-		this.player = GetComponent<Player>();
-		this.actionCollider = this.transform.GetChild(0).GetComponent<OrbitingAction>();
+		player = GetComponent<Player>();
+		actionCollider = this.transform.GetChild(0).GetComponent<OrbitingAction>();
 		timer = GlobalCooldown;
+		spriteRenderer = this.GetComponent<SpriteRenderer>();
+		normalSprite = spriteRenderer.sprite;
+		animator = this.GetComponent<Animator>();
+
 	}
 
 	void Update()
 	{ 
-		// check if we're on the global cooldown - to prevent spamming
-		if ((timer += Time.deltaTime) >= GlobalCooldown)
+		if (!isStunned) // can't do anything
 		{
-			if (Input.GetButtonDown("Tag"+player.Number) && this.transform.childCount < 2)
+			// check if we're on the global cooldown - to prevent spamming
+			if ((timer += Time.deltaTime) >= GlobalCooldown)
 			{
-				actionCollider.PerformAction(RuleManager.Verbtype.Tag, true);
-				timer = 0;
+				if (Input.GetButtonDown("Tag"+player.Number) && this.transform.childCount < 2)
+				{
+					actionCollider.PerformAction(RuleManager.Verbtype.Tag, true);
+					timer = 0;
+				}
+				else if (Input.GetButton("Kick"+player.Number))
+				{
+					actionCollider.PerformAction(RuleManager.Verbtype.Kick, true);
+					timer = 0;
+				}
+				else if (Input.GetButtonDown("Grab"+player.Number) && this.transform.childCount < 2 && !isGrabbed)
+				{
+					actionCollider.PerformAction(RuleManager.Verbtype.Grab, true);
+					timer = 0;
+				}
+				else if (Input.GetButtonDown ("Dash"+player.Number) && !isGrabbed)
+				{
+					Dash(new Vector2(Input.GetAxis ("Horizontal"+player.Number), Input.GetAxis("Vertical"+player.Number)));
+					timer = 0;
+				}
 			}
-			else if (Input.GetButton("Kick"+player.Number))
-			{
-				actionCollider.PerformAction(RuleManager.Verbtype.Kick, true);
-				timer = 0;
-			}
-			else if (Input.GetButtonDown("Grab"+player.Number) && this.transform.childCount < 2 && !isGrabbed)
-			{
-				actionCollider.PerformAction(RuleManager.Verbtype.Grab, true);
-				timer = 0;
-			}
-			else if (Input.GetButtonDown ("Dash"+player.Number) && !isGrabbed)
-			{
-				Dash(new Vector2(Input.GetAxis ("Horizontal"+player.Number), Input.GetAxis("Vertical"+player.Number)));
-				timer = 0;
-			}
+		}
+		else if ((stunnedTimer += Time.deltaTime) > StunnedTime)
+		{
+			isStunned = false;
+			stunnedTimer = 0;
+			UseStunnedSprite(false);
+		}
+		// release the tag hand
+		if (Input.GetButtonUp("Tag"+player.Number) && this.transform.childCount < 2)
+		{
+			actionCollider.PerformAction(RuleManager.Verbtype.Tag, false);
 		}
 		if (this.transform.childCount > 1) // we're holding onto something..
 			isGrabbing = true;
@@ -70,7 +95,10 @@ public class PlayerBehaviour : MonoBehaviour, Noun
 
 		// if our grabber has taken enough kicks, free ourselves
 		if (grabber != null && grabber.gameObject.GetComponent<PlayerBehaviour>().hasTakenEnoughKicks())
+		{
 			isGrabbed = false;
+			UseStunnedSprite(false);
+		}
 	}
 
 	void FixedUpdate()
@@ -82,21 +110,33 @@ public class PlayerBehaviour : MonoBehaviour, Noun
 
 		/*rigidbody2D.velocity = new Vector2(Mathf.Lerp(0, input.x * curSpeed, SpeedUpTime),
 		                                   Mathf.Lerp(0, input.y * curSpeed, SpeedUpTime));*/
-		rigidbody2D.AddForce(input * curSpeed, ForceMode2D.Force);
+		if (!isStunned)
+			rigidbody2D.AddForce(input * curSpeed, ForceMode2D.Force);
 
 		if (isGrabbed) // pull toward your grabber
 		{
 			Vector3 direction = this.transform.position - grabber.position;
-			if (direction.magnitude < 0.40f)
+			if (direction.magnitude < 0.45f)
 				direction = Vector3.zero;
 			this.rigidbody2D.AddForce(new Vector2(direction.x, direction.y) * LaunchSpeed/-7.0f);
 		}
+	}
+
+	private void UseStunnedSprite(bool stunned)
+	{
+		if (stunned)
+			spriteRenderer.sprite = StunnedSprite;
+		else
+			spriteRenderer.sprite = normalSprite;
+		// need to stop the animator or it will overwrite any changes
+		animator.enabled = !stunned;
 	}
 
 	// launch in the direction
 	public void Kicked(int player, Vector3 direction)
 	{
 		isGrabbed = false;
+		UseStunnedSprite(false);
 		rigidbody2D.AddForce(direction.normalized * LaunchSpeed);
 		kicksTaken++;
 		AudioManager.Instance.PlayKickPlayer();
@@ -104,9 +144,10 @@ public class PlayerBehaviour : MonoBehaviour, Noun
 
 	public void Tagged(int player)
 	{
-		//TODO: THIS
-		StartCoroutine("Stunned");
 		AudioManager.Instance.PlayTagPlayer();
+		isStunned = true;
+		stunnedTimer = 0;
+		UseStunnedSprite(true);
 	}
 
 	public void Grabbed(int player)
@@ -116,10 +157,12 @@ public class PlayerBehaviour : MonoBehaviour, Noun
 		GameObject grabberGO = Camera.main.GetComponent<RuleManager>().players[player-1];
 		grabberGO.GetComponent<PlayerBehaviour>().kicksTaken = 0;
 		grabber = grabberGO.transform;
+		UseStunnedSprite(true);
 	}
 
 	IEnumerator Stunned()
 	{
+		AudioManager.Instance.PlayTagPlayer();
 		float timer = 0;
 		while (timer < StunnedTime)
 		{
@@ -131,7 +174,7 @@ public class PlayerBehaviour : MonoBehaviour, Noun
 
 	public void Dash(Vector2 input)
 	{
-		print ("dash");
+		AudioManager.Instance.PlayDash();
 		this.rigidbody2D.AddForce (input.normalized * LaunchSpeed/2.0f);
 	}
 
