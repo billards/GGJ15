b3597e7f1836f9e0fef51f0c3518967cde5ed3e6
Bill Kalominidis <billkalominidis@gmail.com>
2015-01-24 11:18:10 +1100

    Initial commit with scene

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Movement : MonoBehaviour 
+{
+	// Normal Movements Variables
+	private float walkSpeed;
+	private float curSpeed;
+	private float maxSpeed;
+
+	public float WalkSpeed = 5.0f;
+	public float SpeedUpTime = 1.0f;
+	
+	void Start()
+	{
+	}
+	
+	void FixedUpdate()
+	{
+		curSpeed = WalkSpeed;
+		maxSpeed = curSpeed;
+
+		// Move senteces
+		rigidbody2D.velocity = new Vector2(Mathf.Lerp(0, Input.GetAxis("Horizontal") * curSpeed, SpeedUpTime),
+		                                   Mathf.Lerp(0, Input.GetAxis("Vertical") * curSpeed, SpeedUpTime));
+	}
+}
