d19b56e0246a0d12f5617a031b48f0c52a47f567
Liam Parker <parker.liam5@gmail.com>
2015-01-24 12:00:31 +1100

    Update
    - Added switch
    - Still needs further implementation

---
+++ Assets/Scripts/Switch.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Switch : MonoBehaviour {
+
+    private bool isActivated = false;
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (isActivated)
+            Remove();
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // Activate new Rule
+
+        isActivated = true;
+        Debug.Log("Switch Activated");
+    }
+
+    private void Remove()
+    {
+        DestroyObject(this.gameObject);
+
+        //Visual effects for switch removal
+    }
+    
+}
